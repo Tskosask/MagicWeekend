85aca02ad6134467e7730c4322bb188a846e28a7
Tyler Kosaski <tskosask@gmail.com>
2017-07-02 17:55:54 -0400

    Added Fire and Water Spells, Fixed Health Script, added hurt overlay.

--- Assets/Scripts/EarthDestruction.cs
+++ Assets/Scripts/EarthDestruction.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class EarthDestruction : MonoBehaviour {
 
     public GameObject brokenEarth;
     private int maxPieces = 15;
+    private Health health;
+
+    private void Start()
+    {
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        int pieces = 1;
-        while (pieces < maxPieces)
-        {
-            GameObject brokenPiece = Instantiate(brokenEarth, new Vector3(transform.position.x, transform.position.y, transform.position.z), new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w));
-            Destroy(brokenPiece, Random.Range(1f, 6f)); //pieces go away after 5 seconds
-            ++pieces;
-        }
+        //int pieces = 1;
+        //while (pieces < maxPieces)
+        //{
+        //    float randRot = Random.Range(0f, 180f);
+        //    GameObject brokenPiece = Instantiate(brokenEarth, new Vector3(transform.position.x, transform.position.y, transform.position.z), new Quaternion(randRot, randRot, randRot, randRot));  //new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w));
+        //    Destroy(brokenPiece, Random.Range(1f, 6f)); //pieces go away after 5 seconds
+        //    ++pieces;
+        //}
+
+        health.TakeDamage(25);
 
         Destroy(this.gameObject);
     }
