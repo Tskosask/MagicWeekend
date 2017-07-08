fd028b0c912d37b071847ec8c8de6265ca14319f
Tyler Kosaski <tskosask@gmail.com>
2017-07-08 11:45:25 -0400

    Made the air spell look good and added a basic destruction script.

---
+++ Assets/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float lowRange = 1f;
+        float highRange = 2f;
+
+        //rotate randomly every frame?
+        transform.Rotate(0, 0, Random.Range(lowRange, highRange));
+	}
+}
